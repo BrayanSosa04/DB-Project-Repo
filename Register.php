ae929fef26e884e5d6e43576285356867eb3c7b1
Brayan Sosa <101683480+BrayanSosa04@users.noreply.github.com>
2023-04-13 18:28:12 -0500

    fixed register

--- Register.php
+++ Register.php
@@ -24,37 +24,37 @@
                     <ul className='boxes-container'>
                         <li className='guest-info'>
                             <label className='Label'>Username:</label>
-                            <input id='username_phy' className='inputbox' type='text' minLength="2" maxLength="50" title='Please enter the username you will use to sign in.' required placeholder='Enter a desired username.'
+                            <input id='username_phy' className='username_phy' type='text' minLength="2" maxLength="50" title='Please enter the username you will use to sign in.' required placeholder='Enter a desired username.'
                             />
                         </li>
                         <li className='guest-info'>
                             <label className='Label'>Password:</label>
-                            <input id='password_phy' className='inputbox' type='password' minLength="2" maxLength="50" title='Please enter the password you will use to sign in.' required placeholder='Enter a password.'
+                            <input id='password_phy' className='password_phy' type='password' minLength="2" maxLength="50" title='Please enter the password you will use to sign in.' required placeholder='Enter a password.'
                         />
                         </li>
                         <li className='guest-info'>
                             <label className='Label'>Full Name:</label>
-                            <input  id='fullname_phy' className='fullname' type='text' title='Please enter your full name.' minLength="4" maxLength="25" required placeholder='Enter your full name.'
+                            <input  id='fullname_phy' className='fullname_phy' type='text' title='Please enter your full name.' minLength="4" maxLength="25" required placeholder='Enter your full name.'
                         />
                         </li>
                         <li className='guest-info'>
                             <label className='Label'>Email:</label>
-                            <input id='email_phy' className='inputbox' type='text' title='Please enter your email.' minLength="4" maxLength="25" required placeholder='Enter your email.'
+                            <input id='email_phy' className='email_phy' type='text' title='Please enter your email.' minLength="4" maxLength="25" required placeholder='Enter your email.'
                         />
                         </li>
                         <li className='guest-info'>
                             <label className='Label'>Phone Number:</label>
-                            <input id='phone_number_phy' className='inputbox' type='text' title='Please enter your phone number.' minLength="10" maxLength="10" required placeholder='Enter your phone number.'
+                            <input id='phone_number_phy' className='phone_number_phy' type='text' title='Please enter your phone number.' minLength="10" maxLength="10" required placeholder='Enter your phone number.'
                         />
                         </li>
                         <li className='guest-info'>
                             <label className='Label'>Date of Birth:</label>
-                            <input id='date_of_birth_phy' className='inputbox' type='text' title='Please enter your phone number.' minLength="4" maxLength="10" required placeholder='Enter your date of birth.'
+                            <input id='date_of_birth_phy' className='date_of_birth_phy' type='text' title='Please enter your phone number.' minLength="4" maxLength="10" required placeholder='Enter your date of birth.'
                         />
                         </li>
                         <li className='guest-info'>
                             <label className='Label'>Sex:</label>
-                            <input id= 'sex_phy' className='inputbox' type='text' title='Please enter your phone number.' minLength="1" maxLength="1" required placeholder='Enter your sex (M, F, O).'
+                            <input id= 'sex_phy' className='sex_phy' type='text' title='Please enter your phone number.' minLength="1" maxLength="1" required placeholder='Enter your sex (M, F, O).'
                         />
                         </li>
                     </ul>
@@ -73,42 +73,42 @@
                         <ul className='boxes-container'>
                             <li className='guest-info'>
                                 <label className='Label'>Username:</label>
-                                <input id='username_spec' className='inputbox' type='text' minLength="2" maxLength="50" title='Please enter the username you will use to sign in.' required placeholder='Enter a desired username.'
+                                <input id='username_spec' className='username_spec' type='text' minLength="2" maxLength="50" title='Please enter the username you will use to sign in.' required placeholder='Enter a desired username.'
                             />
                             </li>
                             <li className='guest-info'>
                                 <label className='Label'>Password:</label>
-                                <input id='password_pass' className='inputbox' type='password' minLength="2" maxLength="50" title='Please enter the password you will use to sign in.' required placeholder='Enter a password.'
+                                <input id='password_pass' className='password_pass' type='password' minLength="2" maxLength="50" title='Please enter the password you will use to sign in.' required placeholder='Enter a password.'
                             />
                             </li>
                             <li className='guest-info'>
                                 <label className='Label'>Full Name:</label>
-                                <input id = 'fullname_spec' className='inputbox' type='text' title='Please enter your full name.' minLength="4" maxLength="25" required placeholder='Enter your full name.'
+                                <input id = 'fullname_spec' className='fullname_spec' type='text' title='Please enter your full name.' minLength="4" maxLength="25" required placeholder='Enter your full name.'
                             />
                             </li>
                             <li className='guest-info'>
                                 <label className='Label'>Email:</label>
-                                <input id = 'email_spec' className='inputbox' type='text' title='Please enter your email.' minLength="4" maxLength="25" required placeholder='Enter your email.'
+                                <input id = 'email_spec' className='email_spec' type='text' title='Please enter your email.' minLength="4" maxLength="25" required placeholder='Enter your email.'
                             />
                             </li>
                             <li className='guest-info'>
                                 <label className='Label'>Phone Number:</label>
-                                <input id = 'phone_number_spec' className='inputbox' type='text' title='Please enter your phone number.' minLength="10" maxLength="10" required placeholder='Enter your phone number.'
+                                <input id = 'phone_number_spec' className='phone_number_spec' type='text' title='Please enter your phone number.' minLength="10" maxLength="10" required placeholder='Enter your phone number.'
                             />
                             </li>
                             <li className='guest-info'>
                                 <label className='Label'>Date of Birth:</label>
-                                <input id = 'date_of_birth_spec' className='inputbox' type='text' title='Please enter your phone number.' minLength="4" maxLength="10" required placeholder='Enter your date of birth.'
+                                <input id = 'date_of_birth_spec' className='date_of_birth_spec' type='text' title='Please enter your phone number.' minLength="4" maxLength="10" required placeholder='Enter your date of birth.'
                             />
                             </li>
                             <li className='guest-info'>
                                 <label className='Label'>Specialist Practice:</label>
-                                <input id='specialist_practice' className='inputbox' type='text' title='Please enter your phone number.' minLength="4" maxLength="50" required placeholder='Enter your specialty of practice.'
+                                <input id='specialist_practice' className='specialist_practice' type='text' title='Please enter your phone number.' minLength="4" maxLength="50" required placeholder='Enter your specialty of practice.'
                             />
                             </li>
                             <li className='guest-info'>
                                 <label className='Label'>Sex:</label>
-                                <input id ='sex_spec' className='inputbox' type='text' title='Please enter your phone number.' minLength="1" maxLength="1" required placeholder='Enter your sex (M, F, O).'
+                                <input id ='sex_spec' className='sex_spec' type='text' title='Please enter your phone number.' minLength="1" maxLength="1" required placeholder='Enter your sex (M, F, O).'
                             />
                             </li>
                         </ul>
@@ -119,7 +119,6 @@
                 </div> 
             </form>
           </div>
-
           <div id="PAT" class="tabContent">
             <form action = "RegisterPAT.php" method = "POST">
                 <div className= 'Register'>
@@ -128,41 +127,52 @@
                         <ul className='boxes-container'>
                             <li className='guest-info'>
                                 <label className='Label'>Username:</label>
-                                <input id='username_pat' name = 'username_pat' className='inputbox' type='text' minLength="2" maxLength="50" title='Please enter the username you will use to sign in.' required placeholder='Enter a desired username.'
+                                <input id='username_pat' className='username_pat' type='text' minLength="2" maxLength="50" title='Please enter the username you will use to sign in.' required placeholder='Enter a desired username.'
                             />
                             </li>
                             <li className='guest-info'>
                                 <label className='Label'>Password:</label>
-                                <input id='password_pat' name = 'password_pat' className='inputbox' type='password' minLength="2" maxLength="50" title='Please enter the password you will use to sign in.' required placeholder='Enter a password.'
+                                <input id='password_pat' className='password_pat' type='password' minLength="2" maxLength="50" title='Please enter the password you will use to sign in.' required placeholder='Enter a password.'
                             />
                             </li>
                             <li className='guest-info'>
                                 <label className='Label'>Full Name:</label>
-                                <input id='fullname_pat' name='fullname_pat' className='inputbox' type='text' title='Please enter your full name.' minLength="4" maxLength="25" required placeholder='Enter your full name.'
+                                <input id='fullname_pat' className='fullname_pat' type='text' title='Please enter your full name.' minLength="4" maxLength="25" required placeholder='Enter your full name.'
                             />
                             </li>
                             <li className='guest-info'>
                                 <label className='Label'>Email:</label>
-                                <input id='email_pat' name='email_pat' className='inputbox' type='text' title='Please enter your email.' minLength="4" maxLength="50" required placeholder='Enter your email.'
+                                <input id='email_pat' className='email_pat' type='text' title='Please enter your email.' minLength="4" maxLength="50" required placeholder='Enter your email.'
                             />
                             </li>
                             <li className='guest-info'>
                                 <label className='Label'>Phone Number:</label>
-                                <input id='phonenumber_pat' name='phonenumber_pat' className='inputbox' type='text' title='Please enter your phone number.' minLength="10" maxLength="10" required placeholder='Enter your phone number.'
+                                <input id='phonenumber_pat' className='phonenumber_pat' type='text' title='Please enter your phone number.' minLength="10" maxLength="10" required placeholder='Enter your phone number.'
                             />
                             </li>
                             <li className='guest-info'>
                                 <label className='Label'>Date of Birth:</label>
-                                <input id='date_of_birth_pat' name='date_of_birth_pat' className='inputbox' type='text' title='Please enter your phone number.' minLength="4" maxLength="10" required placeholder='Enter your date of birth.'
+                                <input id='date_of_birth_pat' className='date_of_birth_pat' type='text' title='Please enter your phone number.' minLength="4" maxLength="10" required placeholder='Enter your date of birth.'
                             />
                             </li>
                             <li className='guest-info'>
                                 <label className='Label'>Address:</label>
-                                <input id='address_pat' name='address_pat' className='inputbox' type='text' title='Please enter your phone number.' minLength="4" maxLength="60" required placeholder='Enter your address.'
+                                <input id='address_pat' className='address_pat' type='text' title='Please enter your phone number.' minLength="4" maxLength="60" required placeholder='Enter your address.'
                             />
+                            </li>
                             <li className='guest-info'>
-                                <label className='Label'>Today's Date:</label>
-                                <input id='Today_Date_pat' name='Today_Date_pat' className='inputbox' type='date' title='Please enter the current date.' required placeholder='Enter the current date'
+                                <label className='Label'>Billing Information:</label>
+                                <input id='billing_info_pat' className='billing_info_pat' type='text' title='Please enter your phone number.' minLength="4" maxLength="60" required placeholder='Enter your billing information.'
+                            />
+                            </li>
+                            <li className='guest-info'>
+                                <label className='Label'>Sex:</label>
+                                <input id='sex_pat' className='sex_pat' type='text' title='Please enter your phone number.' minLength="1" maxLength="1" required placeholder='Enter your sex (M, F, O).'
+                            />
+                            </li>
+                            <li className='guest-info'>
+                                <label className='Label'>Insurance ID:</label>
+                                <input id='insurance_ID_pat' className='insurance_ID_pat' type='text' title='Please enter your phone number.' minLength="4" maxLength="20" required placeholder='Enter your insurance ID.'
                             />
                             </li>
                         </ul>
@@ -173,9 +183,16 @@
                 </div>
             </form>
           </div>
-          
     </div>
 
 
 </body>
 </html>
+
+
+<!-- if(invalidID(username_pat) !== false) {
+        header("location: ../Register.php?error=invalidID);
+        echo
+        "<script> alert('Wrong password'); <script>"
+    }
+    
