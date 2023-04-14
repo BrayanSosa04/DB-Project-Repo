d47e7092ce1323fa5fb1daaa9e78b7705f1db1ca
Brayan Sosa <101683480+BrayanSosa04@users.noreply.github.com>
2023-04-13 19:56:20 -0500

    login css

--- Login.php
+++ Login.php
@@ -1,34 +1,40 @@
+<!LOGIN html>
+<html>
+
 <head>
-<link rel="stylesheet" href="Login.css?v=<?php echo time(); ?>">
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="Login.css">
 </head>
-<body>
-<div className="Login">
+
+<div>
+    <p class="title"><span><b>Log In</b></span></p>
+</div>
+
+<div class="Login">
     <form action='Login.inc.php' method='POST'>
-            <ul className="information-boxes">
-                <label className='container-title'>LOGIN</label>
-                <ul className='boxes-container'>
-                    <li className='guest-info'>
-                        <label className="Label">Username:</label>
-                        <input id="username_pat" name="username_pat" className="username_pat" type="text" title="Please enter your username." minLength="2" maxLength="50" required placeholder="Enter your username."
-                        />
-                        
-                    </li>
-                    <li className='guest-info'>
-                        <label className="Label">Password:</label>
-                        <input id="password_pat" name="password_pat" className="password_pat" type="password" title="Please enter your password." minLength="2" maxLength="50" required placeholder="Enter your password."
-                        />
-                    </li>
-                </ul>
-                <li>
-                    <button className="Submit" data-testid="button" type="submit">Login</button>
-                </li>
-                <div className="Login-Options">
-                    <p>Don't have an account?
-                        <br/><a href="Register.php">Sign up here</a>.
-                    </p>
-                </div>
-            </ul>
+        <table class="information-boxes">
+            <tr>
+                <th><label className="Label">Username:</label></th>
+                <td><input id="username_pat" className="username_pat" type="text" title="Please enter your username." minLength="2" maxLength="50" required placeholder="Enter your username."></td>
+            </tr>                            
+            <tr>
+                <th><label className="Label">Password:</label></th>
+                <td><input id="password_pat" className="password_pat" type="password" title="Please enter your password." minLength="2" maxLength="50" required placeholder="Enter your password."></td>
+            </tr>
+        </table>
+</div>    
+
+<div class ="button">
+    <button type="submit" class="button" name="submit">Login</button>
+                    <!--</li>
+                    <div className="Login-Options">
+                        <p>Don't have an account?
+                            <br/><a href="Register.php">Sign up here</a>.
+                        </p>
+                    </div>-->
+</div>
     </form>
-    </div>
-</body>
+</div>
+
 </html>
