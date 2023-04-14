319972e945809ad58901bd24dbd757178866c6a7
Brayan Sosa <101683480+BrayanSosa04@users.noreply.github.com>
2023-04-13 19:32:25 -0500

    pls fix register

--- Register.php
+++ Register.php
@@ -5,27 +5,32 @@
     <title>Clinic Database</title>
 </head>
 <body>
-    <form action = "RegisterPat.php" method = "POST">
+    <form action = "RegisterPAT.php" method = "POST">
         <label for = "username_pat">Username:</label>
-        <input type = "text" id = "username_pat" username_pat = "username_pat" name = "username_pat" maxlength = "7">No longer than 7 characters</input>
+        <input type = "text" id = "username_pat" username_pat = "username_pat" name = "username_pat" maxlength = "7"></input>
 
         <label for = "password_pat">Password:</label>
-        <input type = "text" id = "password_pat" password_pat = "password_pat" name = "password_pat" maxlength = "30">No longer than 30 characters</input>
+        <input type = "text" id = "password_pat" password_pat = "password_pat" name = "password_pat" maxlength = "30"></input>
+
+        <br></br>
 
         <label for = "date_of_birth_pat">Date of Birth:</label>
         <input type = "date" id = "date_of_birth_pat" date_of_birth_pat = "date_of_birth_pat" name = "date_of_birth_pat"></input>
 
         <label for = "address_pat">Address:</label>
-        <input type = "text" id = "address_pat" address_pat = "address_pat" name = "address_pat" maxlength = "40">No longer than 40 characters</input>
+        <input type = "text" id = "address_pat" address_pat = "address_pat" name = "address_pat" maxlength = "40"></input>
 
         <label for = "phonenumber_pat">Phone Number:</label>
-        <input type = "text" id = "phonenumber_pat" phonenumber_pat = "phonenumber_pat" name = "phonenumber_pat" maxlength = "10">XXX-XXX-XXXX</input>
+        <input type = "text" id = "phonenumber_pat" phonenumber_pat = "phonenumber_pat" name = "phonenumber_pat" maxlength = "10"></input>
+
+        <br></br>
 
         <label for = "email_pat">Email:</label>
-        <input type = "text" id = "email_pat" email_pat = "email_pat" name = "email_pat" maxlength = "30">john@gmail.com</input>
+        <input type = "text" id = "email_pat" email_pat = "email_pat" name = "email_pat" maxlength = "30"></input>
 
         <label for = "Today_Date_pat">Todays Date:</label>
         <input type = "date" id = "Today_Date_pat" Today_Date_pat = "Today_Date_pat" name = "Today_Date_pat"></input>
+        <br></br>
         <button type = "submit" name = "submit" >Finish Registering</button>
     </form>
 </body>
