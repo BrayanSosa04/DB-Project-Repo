0d6b2f48fabb9d88695cd1e2da3ac3e51b7f895a
Brayan Sosa <101683480+BrayanSosa04@users.noreply.github.com>
2023-04-17 21:59:11 -0500

    creating an office

---
+++ office_create.php
@@ -0,0 +1,39 @@
+<form action = "CEO.php">
+<style>
+    body {
+      background-color: lightgray;
+    }
+
+    h1 {
+      font-family: "Times New Roman";
+      text-align: center;
+      background-color: mediumseagreen;
+      border: 4px solid green;
+      border-style: double;
+  }
+</style>
+
+<body>
+  <h1>Create New Office</h1>
+
+  <section id = "OSVCOverview">
+    <form action = "office_create2.php" method = "POST">
+      <label for = "OSVCOffice_ID">Office ID:</label>
+      <input type = "text" id = "OSVCOffice_ID" OSVCOffice_ID = "OSVCOffice_ID" name = "OSVCOffice_ID">
+
+      <label for = "OSVCAddress">Address:</label>
+      <input type = "text" id = "OSVCAddress" OSVCAddress = "OSVCAddress" name = "OSVCAddress">
+
+      <label for = "OSVCDate">Date Estabished:</label>
+      <input type = "text" id = "OSVCDate" OSVCDate = "OSVCDate" name = "OSVCDate">
+
+      <label for = "OSVCBudget">Budget Allocation:</label>
+      <input type = "text" id = "OSVCBudget" OSVCBudget = "OSVCBudget" name = "OSVCBudget">
+
+      <button type = "submit" id = "OSVCsubmit" name = "OSVCsubmit">Add Office</button>
+    </form>
+  </section>
+</body>
+
+<button type = "submit" name = "submit">Return to CEO page</button>
+</form>
---
+++ office_create2.php
@@ -0,0 +1,29 @@
+<form action = "office_create.php">
+<style>
+  body {
+    background-color: lightgray;
+  }
+</style>
+<body>
+<?php
+  include_once 'connectToTheDB.php';
+
+  $OSVCOffice_ID = $_GET['OSVCOffice_ID'];
+  $OSVCAddress = $_GET['OSVCAddress'];
+  $OSVCDate = $_GET['OSVCDate'];
+  $OSVCBudget = $_GET['OSVCBudget'];
+
+  $sql = "INSERT INTO offices(Office_ID, Address, Date_Established, Budget)
+  VALUES ('$OSVCOffice_ID', '$OSVCAddress', '$OSVCDate', '$OSVCBudget');";
+  $result = mysqli_query($conn, $sql);
+
+  if($result) {
+    echo 'New Office Created!';
+  }
+  else {
+    echo 'Office Creation Failed';
+  }
+?>
+<button type = "submit" name = "submit">Return to CEO page</button>
+</body>
+</form>
