f23d36d2e0fd84b6764e586abf04b577a71ffe9a
Brayan Sosa <101683480+BrayanSosa04@users.noreply.github.com>
2023-04-18 19:59:57 -0500

    testing delete physician

---
+++ deletePhysiciansucc.php
@@ -0,0 +1,18 @@
+<?php
+    include_once 'connectToTheDB.php';
+
+    $phy = $_POST['Phy_ID'];
+    $sql = "UPDATE physician SET Flagged_Delete = 1 WHERE Employee_ID = '$phy' AND Flagged_Delete = 0";
+
+    $result = mysqli_query($conn, $sql);
+
+    if($result){
+        echo "Physician has been removed";
+    }
+    else{
+        echo "Sorry we can not remove this physician";
+    }
+?>
+<form action = "CEO.php">
+    <button type = "submit" name = "submit" >Return to the main page</button>
+</form>
