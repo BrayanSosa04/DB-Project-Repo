d45cd6763cd4d8e2d68ca545720baa711f6261af
Brayan Sosa <101683480+BrayanSosa04@users.noreply.github.com>
2023-04-19 08:31:39 -0500

    testing delete department

--- CEO.php
+++ CEO.php
@@ -25,12 +25,13 @@
 
       <label for = "OSVinput2"># of Specialists:</label>
       <input type = "text" id = "OSVinput2" OSVinput = "OSVinput2" name = "OSVinput2"><br></br>
-      <button type = "submit" id = "OSVsubmit" name = "OSVsubmit">View Office Report</button>
+      <button type = "submit" id = "OSVsubmit" name = "OSVsubmit">View Office Report</button><br></br>
       <button type = "submit" id = "OSVcreate" name = "OSVcreate" formaction = "office_create.php">Add Office</button>
       <button type = "submit" id = "OSVupdate" name = "OSVupdate" formaction = "office_update.php"> Update Office</button>
       <button type = "submit" id = "OSVdelete" name = "OSVdelete" formaction = "office_delete.php"> Delete Existing Office</button><br></br>
       <button type = "submit" id = "DeptCreate" name = "DeptCreate" formaction = "department_create.php">Add Department</button>
       <button type = "submit" id = "DeptUpdate" name = "DeptUpdate" formaction = "department_update.php">Update Department</button>
+      <button type = "submit" id = "DeptDelete" name = "DeptDelete" formaction = "department_delete.php">Delete Existing Department</button>
     </form>
   </section>
 
