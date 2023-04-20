da2ffde3642c2a10989c76b2040d6e6a7d35a5cd
Brayan Sosa <101683480+BrayanSosa04@users.noreply.github.com>
2023-04-20 15:15:46 -0500

    Testing last change specialist

--- changeSpecialist.php
+++ changeSpecialist.php
@@ -120,6 +120,7 @@
 
       <label for = "SUpdateSupervisor">Change Supervisor:</label>
       <select id = "SUpdateSupervisor" SUpdateSupervisor = "SUpdateSupervisor" name = "SUpdateSupervisor">
+        <option value = "<?php NULL?>">None Selected</option>
         <?php 
           include_once 'connectToTheDB.php';
 
