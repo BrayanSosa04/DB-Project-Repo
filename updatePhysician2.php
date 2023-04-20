15fe3d52be378bdf531360f61e696e7cede97fbb
Brayan Sosa <101683480+BrayanSosa04@users.noreply.github.com>
2023-04-20 10:38:18 -0500

    pls be fixed

--- updatePhysician2.php
+++ updatePhysician2.php
@@ -14,17 +14,17 @@
 <?php
   include_once 'connectToTheDB.php';
 
-  $SUpdateID = $_POST['$SUpdateID'];
-  $SUpdateName = $_POST['$SUpdateName'];
-  $SUpdateOffice = $_POST['$Spe_Off'];
-  $SUpdateDepartment = $_POST['$Spe_Dept'];
-  $SUpdateSex = $_POST['$SUpdateSex'];
-  $SUpdateEmail = $_POST['$SUpdateEmail'];
-  $SUpdatePhone = $_POST['$SUpdatePhone'];
-  $SUpdateAvailable = $_POST['$SUpdateAvailable'];
-  $SUpdateSalary = $_POST['$SUpdateSalary'];
+  $SUpdateID = $_POST['SUpdateID'];
+  $SUpdateName = $_POST['SUpdateName'];
+  $SUpdateOffice = $_POST['Spe_Off'];
+  $SUpdateDepartment = $_POST['Spe_Dept'];
+  $SUpdateSex = $_POST['SUpdateSex'];
+  $SUpdateEmail = $_POST['SUpdateEmail'];
+  $SUpdatePhone = $_POST['SUpdatePhone'];
+  $SUpdateAvailable = $_POST['SUpdateAvailable'];
+  $SUpdateSalary = $_POST['SUpdateSalary'];
   $SUpdateLast_Modified = date('Y-m-d');
-  $SUpdateModifiedBy = $_POST['$SUpdateModifiedBy'];
+  $SUpdateModifiedBy = $_POST['SUpdateModifiedBy'];
 
   $sql = "UPDATE physician
           SET Employee_Name = '$SUpdateName', Asso_Office = '$SUpdateOffice', Asso_Department = '$SUpdateDepartment', Sex = '$SUpdateSex', Employee_Email = '$SUpdateEmail', Employee_Phone = '$SUpdatePhone', Available = '$SUpdateAvailable', Salary = $SUpdateSalary, Last_Date_Modified = '$SUpdateLast_Modified', Last_Modified_By = '$SUpdateModifiedBy'
